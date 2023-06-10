5a7c65121d2daaa188b6603d2b9368116e69af8d
Maria Eduarda Araujo <maeduardaaraujo01@gmail.com>
2023-06-10 16:03:30 -0300

    commit 3

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-var cards = document.querySelectorAll('.container .card'); // selecionar todos os cards
+var cards = document.querySelectorAll('.container .card');
 
 function slideMensagem() {
   var mensagem = this.querySelector('.mensagem');
   if (!mensagem) {
-    // Cria elemento de texto com mensagem
     mensagem = document.createElement('div');
     mensagem.textContent = 'Este setor é responsável por';
-    mensagem.classList.add('mensagem'); // Adiciona classe para fazer estilização
-
-    // Define o estilo da mensagem
-    mensagem.style.fontSize = '16px';
-    mensagem.style.color = 'white';
-    mensagem.style.textDecoration = 'none';
-    mensagem.style.background = 'rgba(128, 128, 128, 0.8)';
-    mensagem.style.transition = 'width 10s, height 12s, background-color 12s, opacity 12s, transform 12s';
+    mensagem.classList.add('mensagem');
 
     this.appendChild(mensagem);
-
-    // Adicionar classe para iniciar a transição
-    setTimeout(function() {
-      mensagem.classList.add('show');
-    }, 10); // Espera um pequeno intervalo para ativar a transição
   }
 }
 
@@ -32,6 +19,6 @@ function removerMensagem() {
 }
 
 cards.forEach(function(card) {
-  card.addEventListener('mouseover', slideMensagem);
-  card.addEventListener('mouseout', removerMensagem);
+  card.addEventListener('mouseenter', slideMensagem);
+  card.addEventListener('mouseleave', removerMensagem);
 });
